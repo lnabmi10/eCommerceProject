a541156b37347501f713e3f7dce50ec30809dcaa
mohammed <souarimohammed@gmail.com>
2024-01-12 01:19:16 +0100

    delete blog,get blog

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -5,13 +5,14 @@ const valideMongodbId = require('../utils/validateMongodbId')
 
 
 
+
 const createBlog = asyncHandler(async(req,res)=>{
 
     try {
 
         const newBlog = await Blog.create(req.body)
         res.json({
-            status : succes,
+            status : "success",
             newBlog ,
         })
         
@@ -20,6 +21,78 @@ const createBlog = asyncHandler(async(req,res)=>{
     }
 })
 
+const updateBlog = asyncHandler(async(req,res)=>{
+    const {id}=req.params;
 
 
-module.exports={createBlog}
+    try {
+
+        const updatedBlog = await Blog.findByIdAndUpdate(id,req.body,{
+            new : true
+        })
+        res.json(updatedBlog )
+        
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+const getBlog = asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+
+
+    try {
+
+        const getBlog = await Blog.findById(id)
+
+        await Blog.findByIdAndUpdate(id,
+            {
+                $inc : {numberOfViews : 1}
+            },
+            {new:true})
+            res.json(getBlog )
+        
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+
+
+const getAllBlogs = asyncHandler(async(req,res)=>{
+
+
+    try {
+
+        const getBlogs = await Blog.find()
+
+       
+         res.json(getBlogs )
+        
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+const deleteBlog = asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+
+
+    try {
+
+        const deletedBlog = await Blog.findByIdAndDelete(id)
+        res.json(deletedBlog )
+        
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+
+
+
+module.exports={createBlog,updateBlog,getBlog,getAllBlogs,deleteBlog}
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
-const {createBlog}=require('../controller/blogCtrl');
+const {createBlog,updateBlog,getBlog,getAllBlogs,deleteBlog}=require('../controller/blogCtrl');
 const router = express.Router();
 
 
 
 router.post("/createblog", authMiddleware,isAdmin,createBlog)
+router.put("/updateblog/:id", authMiddleware,isAdmin,updateBlog)
+router.delete("/deleteblog/:id", authMiddleware,isAdmin,deleteBlog)
+router.get("/getblog/:id",getBlog)
+router.get("/getallblogs/",getAllBlogs)
 
 
 

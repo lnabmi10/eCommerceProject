c85322d2613f7a8f676316b46f14fb3aaa90ac5e
mohammed <souarimohammed@gmail.com>
2024-01-17 23:43:58 +0100

    update delete category

--- controller/categoryCtrl.js
+++ controller/categoryCtrl.js
@@ -11,5 +11,24 @@ const createCategory = asyncHandler(async(req,res)=>{
         throw new Error(error)
     }})
 
+const updateCategory = asyncHandler(async(req,res)=>{
+    const {id}=req.params
+        try {
+            const updatedCategory = await Category.findByIdAndUpdate(id,req.body,{new:true})
+            res.json(updatedCategory)
+    
+        } catch (error) {
+            throw new Error(error)
+        }})
 
-module.exports = {createCategory}
+const deleteCategory = asyncHandler(async(req,res)=>{
+            const {id}=req.params
+                try {
+                    const deletedCategory = await Category.findByIdAndDelete(id)
+                    res.json(deletedCategory)
+            
+                } catch (error) {
+                    throw new Error(error)
+                }})
+
+module.exports = {createCategory,updateCategory}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
-const {createCategory} = require('../controller/categoryCtrl')
+const {createCategory,updateCategory,deleteCategory} = require('../controller/categoryCtrl')
 const router = express.Router();
 
 
 router.post("/createcategory", authMiddleware,isAdmin,createCategory)
+router.put("/updatecategory", authMiddleware,isAdmin,updateCategory)
+router.delete("/deletecategory", authMiddleware,isAdmin,deleteCategory)
 
 
 
 module.exports = router
-
+ 

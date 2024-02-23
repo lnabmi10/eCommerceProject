7a9b3962e3de8adad78759a7e0bad91e0b5a3a2c
lnabmi <souarimohammed@gmail.com>
2024-02-23 16:15:58 +0100

    user carts

--- controller/cartCtrl.js
+++ controller/cartCtrl.js
@@ -22,12 +22,13 @@ const addToCart = asyncHandler(
             let getPrice = await Product.findById(prodCart.prodId).select("price").exec();
         // check if user alredy have cart
         const cartAlreadyExist = await Cart.findOne({ ordredBy: findUser._id, cartStatus: "not processed" });
+        const addtoTotal = getPrice.price*prodCart.count
 
         if(!cartAlreadyExist){
         
             const newCart = await Cart.create({products : prodCart,
-                cartTotal:(getPrice.price*prodCart.count),
-               ordredBy:findUser._id})
+                cartTotal : addtoTotal,
+               ordredBy : findUser._id})
             
                 let user = await User.findByIdAndUpdate(id,
                     {$push : { cart : newCart._id}},
@@ -38,7 +39,7 @@ const addToCart = asyncHandler(
            
                    //add product to existing cart
           let productsInCart = [...cartAlreadyExist.products]
-          
+          let indexProd = -1
 
           for (let i = 0; i < productsInCart.length ;i++) {
             
@@ -46,27 +47,23 @@ const addToCart = asyncHandler(
               let {prodId , count , color} = productsInCart[i];
               prodId = prodId.toString()
              
-            console.log(productsInCart[i])
-            console.log("etap" + i + "##################################################################")
 
               if(prodId===prodCart.prodId && color === prodCart.color ){
                 
-                         console.log("il vas just augmenter le count")
-                         productsInCart[i].count += count
+                         productsInCart[i].count += prodCart.count
                           console.log(productsInCart[i])
+                          indexProd = i
 
                          }
-
-              else{
-                productsInCart.push(prodCart);
-                i++        
-            }
+        }
+        if(indexProd==-1){
+            productsInCart.push(prodCart)
         }
         
 
             const updatedCart = await Cart.findByIdAndUpdate(cartAlreadyExist._id ,{
                 products:productsInCart,
-                cartTotal: cartAlreadyExist.cartTotal + getPrice.price*prodCart.count,
+                cartTotal: cartAlreadyExist.cartTotal + addtoTotal,
             },{new:true})
         
         
@@ -81,6 +78,22 @@ const addToCart = asyncHandler(
     }
 )
 
+
+
+const getALLuserCart = asyncHandler(async(req,res)=>{
+    const {id} = req.user
+    valideMongodbId(id)
+
+    try {
+    
+        
+
+        
+    } catch (error) {
+        throw new Error(error)
+    }}
+
+)
 
 
 module.exports={addToCart}
